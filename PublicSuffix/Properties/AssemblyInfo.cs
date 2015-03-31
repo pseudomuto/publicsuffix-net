1d91ebc06bab4dc78bf119627dfd0ace3bb965c3
pseudomuto <david.muto@gmail.com>
2015-03-31 17:55:20 -0400

    fixing version to 0.1.0

--- PublicSuffix/Properties/AssemblyInfo.cs
+++ PublicSuffix/Properties/AssemblyInfo.cs
@@ -25,28 +25,18 @@
 // THE SOFTWARE.
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+[assembly: AssemblyTitle("PublicSuffix")]
+[assembly: AssemblyDescription("Domain Name parser based on the Public Suffix List")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("PseudoMuto")]
+[assembly: AssemblyProduct("PublicSuffix")]
+[assembly: AssemblyCopyright("PseudoMuto")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-[assembly: AssemblyTitle ("PublicSuffix")]
-[assembly: AssemblyDescription ("Domain Name parser based on the Public Suffix List")]
-[assembly: AssemblyConfiguration ("Release")]
-[assembly: AssemblyCompany ("PseudoMuto")]
-[assembly: AssemblyProduct ("PublicSuffix")]
-[assembly: AssemblyCopyright ("PseudoMuto")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: ComVisible(false)]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
