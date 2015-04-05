cfe146738d12c39d040cb0233cdc342a83ba4c5f
pseudomuto <david.muto@gmail.com>
2015-04-05 18:33:13 -0400

    using fake to build nuget package

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ PublicSuffix is a .NET domain name parser based on the [Public Suffix List].
 Status](https://travis-ci.org/pseudomuto/publicsuffix-net.svg?branch=master)](https://travis-ci.org/pseudomuto/publicsuffix-net)
 
 [Public Suffix List]: https://publicsuffix.org/
+
+
+## FAKE Commands
+
+This project is configured to use [Fake] for builds. Because the package is installed under
+_packages_, there is a simple wrapper script aptly named `fake`, that will ensure the package is
+installed and forward arguments to the _Fake.exe_ file.
+
+* Clean - `./fake Clean` cleans the build, test and deploy folders
+* Build - `./fake`
+* Build Tests - `./fake BuildTest`
+* Run Tests - `./fake Test` (will trigger BuildTest)
+
+[Fake]: http://fsharp.github.io/FAKE/
--- build.fsx
+++ build.fsx
@@ -13,6 +13,17 @@ let testToolsDir = "./packages/NUnit.Runners.2.6.4/tools"
 let appReferences  = !! "PublicSuffix/**/*.csproj"
 let testReferences = !! "PublicSuffix.Test/**/*.csproj"
 
+// nuget package details
+let packageAuthors = ["PseudoMuto"]
+let packageName = "PublicSuffixList"
+let packageDescription = "A Domain Name parser based on the Public Suffix List"
+let packageSummary = packageDescription
+let packageRoot = deployDir
+let packageDir = packageRoot @@ "PublicSuffix"
+
+// Restore NuGet Packages
+RestorePackages()
+
 Target "Clean" (fun _ ->
   CleanDirs [buildDir; testDir; deployDir]
 )
@@ -36,6 +47,30 @@ Target "Test" (fun _ ->
         OutputFile = testDir + "TestResults.xml" })
 )
 
+Target "Package" (fun _ ->
+  let targetDir = packageDir @@ "lib/net45"
+  CleanDirs [packageRoot; packageDir; targetDir]
+
+  CopyFile targetDir (buildDir @@ "PublicSuffix.dll")
+  CopyFile targetDir (buildDir @@ "PublicSuffix.dll.mdb")
+  CopyFiles packageDir ["README.md"; "LICENSE"]
+
+  NuGet (fun p ->
+    {p with
+      Files = [("lib/net45/**.*", None, None)]
+      Authors = packageAuthors
+      Project = packageName
+      Description = packageDescription
+      Summary = packageSummary
+      OutputPath = packageRoot
+      WorkingDir = packageDir
+      AccessKey = getBuildParamOrDefault "nugetkey" ""
+      Publish = hasBuildParam "nugetkey" }
+  ) "PublicSuffix.nuspec"
+)
+
+Target "Default" DoNothing
+
 "Clean"
   ==> "Build"
   ==> "BuildTest"
@@ -43,4 +78,10 @@ Target "Test" (fun _ ->
 "BuildTest"
   ==> "Test"
 
-RunTargetOrDefault "Build"
+"Test"
+  ==> "Default"
+
+"Build"
+  ==> "Package"
+
+RunTargetOrDefault "Default"
